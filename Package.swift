3cef36ea5d99971c5fa5aca16c1b38be71211267
Nathan Harris <nathan.harris@globant.com>
2019-01-17 16:31:35 -0800

    Update package definition to use alpha of `NIORedis` and `DatabaseKit`

--- Package.swift
+++ Package.swift
@@ -1,16 +1,17 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "RedisKit",
     products: [
-        .library(name: "RedisKit", targets: ["RedisKit"]),
+        .library(name: "RedisKit", targets: ["RedisKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/redis.git", .branch("add-set-commands")),
+        .package(url: "https://github.com/vapor/database-kit.git", .branch("2")),
+        .package(url: "https://github.com/mordil/nio-redis.git", .branch("master"))
     ],
     targets: [
-        .target(name: "RedisKit", dependencies: ["Redis"]),
+        .target(name: "RedisKit", dependencies: ["NIORedis", "DatabaseKit"]),
         .testTarget(name: "RedisKitTests", dependencies: ["RedisKit"]),
     ]
 )
