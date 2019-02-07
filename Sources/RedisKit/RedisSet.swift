b8d4d6d2acd756bb5daf5c217603d1a7a9505e0e
Nathan Harris <nathan@mordil.info>
2019-02-06 19:25:53 -0800

    Disable most of RedisSet until further NIORedis work is done to support methods

--- Sources/RedisKit/RedisSet.swift
+++ Sources/RedisKit/RedisSet.swift
@@ -1,7 +1,8 @@
 import Foundation
-import Redis
+import DatabaseKit
+import NIORedis
 
-public extension DatabaseConnectionPool where Database == ConfiguredDatabase<RedisDatabase> {
+extension ConnectionPool where Database == RedisDatabase {
     /// Creates a `RedisSet` for the provided key, that contains the declared type of elements.
     ///
     ///     let idSet = redis.createSetReference(fromKey: "ids", ofType: Int.self)
@@ -21,7 +22,7 @@ public extension DatabaseConnectionPool where Database == ConfiguredDatabase<Red
     /// - Parameter fromKey: The key to identify the Set in this `RedisClient`.
     /// - Parameter ofType: The `Collection` type this set represents.
     public func createSetReference<C: Collection>(fromKey key: String, ofType type: C.Type) -> RedisSet<C.Element>
-        where C.Element: RedisDataConvertible
+        where C.Element: RESPConvertible
     {
         return RedisSet(identifier: key, using: self)
     }
@@ -30,23 +31,23 @@ public extension DatabaseConnectionPool where Database == ConfiguredDatabase<Red
 /// A reference to a specific Set in a Redis instance.
 ///
 /// https://redis.io/topics/data-types-intro#sets
-public struct RedisSet<Element: RedisDataConvertible> {
+public struct RedisSet<Element: RESPConvertible> {
     private let id: String
-    private let connectionPool: DatabaseConnectionPool<ConfiguredDatabase<RedisDatabase>>
+    private let connectionPool: ConnectionPool<RedisDatabase>
 
     /// - Parameter identifier: The key identifier to reference this set.
     /// - Parameter owner: The owning `Container` creating this set reference.
     /// - Throws: Errors from `Container.connectionPool(to:)`
-    public init(identifier: String, owner container: Container) throws {
-        self.id = identifier
-        self.connectionPool = try container.connectionPool(to: .redis)
-    }
+//    public init(identifier: String, owner container: Container) throws {
+//        self.id = identifier
+//        self.connectionPool = try container.connectionPool(to: .redis)
+//    }
 
     /// - Parameter identifier: The key identifier to reference this set.
     /// - Parameter using: The connection pool to use for interacting with this set reference.
     public init(
         identifier: String,
-        using connectionPool: DatabaseConnectionPool<ConfiguredDatabase<RedisDatabase>>
+        using connectionPool: ConnectionPool<RedisDatabase>
     ) {
         self.id = identifier
         self.connectionPool = connectionPool
@@ -54,98 +55,98 @@ public struct RedisSet<Element: RedisDataConvertible> {
 
     /// Returns the total count of elements in the set.
     /// - Note: In most cases it's better to call `RedisSet.allElements`.
-    var count: Future<Int> {
-        return connectionPool.withConnection { $0.scard(self.id) }
-    }
+//    var count: EventLoopFuture<Int> {
+//        return connectionPool.withConnection { $0.scard(self.id) }
+//    }
 
-    /// A future that resolves all elements in the set - or nil if none found.
-    var allElements: Future<[Element]?> {
-        return connectionPool.withConnection { $0.smembers(self.id) }
-            .map {
-                guard let set = $0.array else { return nil }
-                return try set.map { try Element.convertFromRedisData($0) }
-            }
-    }
+    /// A EventLoopFuture that resolves all elements in the set - or nil if none found.
+//    var allElements: EventLoopFuture<[Element]?> {
+//        return connectionPool.withConnection { $0.smembers(self.id) }
+//            .map {
+//                guard let set = $0.array else { return nil }
+//                return try set.map { try Element.convertFromRedisData($0) }
+//            }
+//    }
 
     /// Checks if the provided element is currently in the set.
-    public func contains(_ element: Element) -> Future<Bool> {
-        guard let data = try? element.convertToRedisData() else {
-            return connectionPool.eventLoop.newFailedFuture(
-                error: RedisError(identifier: "\(#file).\(#function)", reason: "Failed to convert to RedisData: \(element)")
-            )
-        }
-
-        return connectionPool.withConnection { $0.sismember(self.id, item: data) }
-    }
-
-    /// Inserts the provided elements into the set.
-    /// - Note: Values already in the set will be ignored.
-    /// - Important: This resolves `true` if at least 1 element was inserted.
-    @discardableResult
-    public func insert(_ elements: [Element]) -> Future<Bool> {
-        guard let data = try? elements.map({ try $0.convertToRedisData() }) else {
-            return connectionPool.eventLoop.newFailedFuture(
-                error: RedisError(identifier: "\(#file).\(#function)", reason: "Failed to convert to RedisData: \(elements)")
-            )
-        }
-
-        return connectionPool.withConnection { $0.sadd(self.id, items: data) }
-            .map { return $0 > 0 }
-    }
+//    public func contains(_ element: Element) -> EventLoopFuture<Bool> {
+//        guard let data = try? element.convertToRESP() else {
+//            return connectionPool.eventLoop.makeFailedFuture(
+//                RedisError(identifier: "\(#file).\(#function)", reason: "Failed to convert to RedisData: \(element)")
+//            )
+//        }
+//
+//        return connectionPool.withConnection { $0.sismember(self.id, item: data) }
+//    }
 
     /// Inserts the provided elements into the set.
     /// - Note: Values already in the set will be ignored.
     /// - Important: This resolves `true` if at least 1 element was inserted.
-    @discardableResult
-    public func insert(_ elements: Element...) -> Future<Bool> {
-        return insert(elements)
-    }
+//    @discardableResult
+//    public func insert(_ elements: [Element]) -> EventLoopFuture<Bool> {
+//        guard let data = try? elements.map({ try $0.convertToRESP()() }) else {
+//            return connectionPool.eventLoop.makeFailedFuture(
+//                RedisError(identifier: "\(#file).\(#function)", reason: "Failed to convert to RedisData: \(elements)")
+//            )
+//        }
+//
+//        return connectionPool.withConnection { $0.sadd(self.id, items: data) }
+//            .map { return $0 > 0 }
+//    }
+
+    /// Inserts the provided elements into the set.
+    /// - Note: Values already in the set will be ignored.
+    /// - Important: This resolves `true` if at least 1 element was inserted.
+//    @discardableResult
+//    public func insert(_ elements: Element...) -> EventLoopFuture<Bool> {
+//        return insert(elements)
+//    }
 
     /// Removes the provided elements from the set.
     /// - Note: Values not in the set will be ignored.
     /// - Important: This resolves `true` if at least 1 element was removed.
-    @discardableResult
-    public func remove(_ elements: [Element]) -> Future<Bool> {
-        guard let data = try? elements.map({ try $0.convertToRedisData() }) else {
-            return connectionPool.eventLoop.newFailedFuture(
-                error: RedisError(identifier: "\(#file).\(#function)", reason: "Failed to convert to RedisData: \(elements)")
-            )
-        }
-
-        return connectionPool.withConnection { $0.srem(self.id, items: data) }
-            .map { return $0 > 0 }
-    }
+//    @discardableResult
+//    public func remove(_ elements: [Element]) -> EventLoopFuture<Bool> {
+//        guard let data = try? elements.map({ try $0.convertToRESP() }) else {
+//            return connectionPool.eventLoop.makeFailedFuture(
+//                RedisError(identifier: "\(#file).\(#function)", reason: "Failed to convert to RedisData: \(elements)")
+//            )
+//        }
+//
+//        return connectionPool.withConnection { $0.srem(self.id, items: data) }
+//            .map { return $0 > 0 }
+//    }
 
     /// Removes the provided elements from the set.
     /// - Note: Values not in the set will be ignored.
     /// - Important: This resolves `true` if at least 1 element was removed.
-    @discardableResult
-    public func remove(_ elements: Element...) -> Future<Bool> {
-        return remove(elements)
-    }
+//    @discardableResult
+//    public func remove(_ elements: Element...) -> EventLoopFuture<Bool> {
+//        return remove(elements)
+//    }
 
     /// Removes all values within this set.
     /// - Important: This resolves `true` only if the set was not empty.
     @discardableResult
-    public func removeAll() -> Future<Bool> {
+    public func removeAll() -> EventLoopFuture<Bool> {
         return connectionPool.withConnection { $0.delete(self.id) }
             .map { return $0 > 0 }
     }
 
     /// Randomly selects an element and removes it from the set.
-    public func popRandom() -> Future<Element?> {
-        return connectionPool.withConnection { $0.spop(self.id) }
-            .map {
-                guard !$0.isNull else { return nil }
-                return try Element.convertFromRedisData($0)
-            }
-    }
+//    public func popRandom() -> EventLoopFuture<Element?> {
+//        return connectionPool.withConnection { $0.spop(self.id) }
+//            .flatMap {
+//                guard !$0.isNull else { return nil }
+//                return try Element.convertFromRESP($0)
+//            }
+//    }
 
     /// Randomly selects a single element.
-    public func random() -> Future<Element?> {
-        return connectionPool.withConnection { $0.srandmember(self.id) }
-            .map { return try Element.convertFromRedisData($0) }
-    }
+//    public func random() -> EventLoopFuture<Element?> {
+//        return connectionPool.withConnection { $0.srandmember(self.id) }
+//            .map { return try Element.convertFromRESP($0) }
+//    }
 
     /// Randomly selects random elements, up to the `max` specified.
     ///
@@ -158,16 +159,16 @@ public struct RedisSet<Element: RedisDataConvertible> {
     ///
     /// - Parameter max: The max number of elements to pull, as available.
     /// - Parameter allowDuplicates: Should duplicate elements be picked?
-    public func random(max: Int = 1, allowDuplicates: Bool = false) -> Future<[Element]?> {
-        precondition(max > 0, "Max should be a positive value. Use 'allowDuplicates' to handle proper value sign")
-
-        guard max > 1 else { return random() }
-
-        let count = allowDuplicates ? -max : max
-        return connectionPool.withConnection { $0.srandmember(self.id, max: count) }
-            .map {
-                guard let results = $0.array else { return nil }
-                return try results.map { try Element.convertFromRedisData($0) }
-            }
-    }
+//    public func random(max: Int = 1, allowDuplicates: Bool = false) -> EventLoopFuture<[Element]?> {
+//        precondition(max > 0, "Max should be a positive value. Use 'allowDuplicates' to handle proper value sign")
+//
+//        guard max > 1 else { return random() }
+//
+//        let count = allowDuplicates ? -max : max
+//        return connectionPool.withConnection { $0.srandmember(self.id, max: count) }
+//            .map {
+//                guard let results = $0.array else { return nil }
+//                return try results.map { try Element.convertFromRESP($0) }
+//            }
+//    }
 }
