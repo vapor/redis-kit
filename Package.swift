7aa574d9f66009abca99a8da3d6689261bfcb1ea
Nathan Harris <nathan@mordil.info>
2019-03-26 09:43:55 -0700

    Pull in `swift-log` and depend NIORedis branch for in-dev work

--- Package.swift
+++ Package.swift
@@ -7,11 +7,12 @@ let package = Package(
         .library(name: "RedisKit", targets: ["RedisKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/mordil/nio-redis.git", .branch("master")),
+        .package(url: "https://github.com/apple/swift-log.git", .branch("master")),
+        .package(url: "https://github.com/mordil/nio-redis.git", .revision("remove-driver")),
         .package(url: "https://github.com/vapor/nio-kit.git", .branch("master")),
     ],
     targets: [
-        .target(name: "RedisKit", dependencies: ["NIOKit", "NIORedis"]),
+        .target(name: "RedisKit", dependencies: ["NIOKit", "NIORedis", "Logging"]),
         .testTarget(name: "RedisKitTests", dependencies: ["RedisKit"]),
     ]
 )
