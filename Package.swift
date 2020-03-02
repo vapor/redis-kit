75bb189367bd9783906c518948a79de244fa9854
tanner0101 <me@tanner.xyz>
2020-03-02 18:14:30 -0500

    swift + macos reqs

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "redis-kit",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "RedisKit", targets: ["RedisKit"])
@@ -12,10 +12,17 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://gitlab.com/mordil/swift-redi-stack.git", from: "1.0.0-alpha.7"),
-        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-rc"),
     ],
     targets: [
-        .target(name: "RedisKit", dependencies: ["AsyncKit", "RediStack", "Logging"]),
-        .testTarget(name: "RedisKitTests", dependencies: ["RedisKit", "RediStackTestUtils"]),
+        .target(name: "RedisKit", dependencies: [
+            .product(name: "AsyncKit", package: "async-kit"),
+            .product(name: "RediStack", package: "swift-redi-stack"),
+            .product(name: "Logging", package: "swift-log"),
+        ]),
+        .testTarget(name: "RedisKitTests", dependencies: [
+            .target(name: "RedisKit"),
+            .product(name: "RediStackTestUtils", package: "swift-redi-stack"),
+        ]),
     ]
 )
