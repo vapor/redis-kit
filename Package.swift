9ef8f9dc472d022c4e94e5388ccf3c87ad83e2cb
Nathan Harris <nathan.harris@peek.com>
2020-03-12 11:15:28 -0700

    Update to RediStack Alpha 8 (#31)

--- Package.swift
+++ Package.swift
@@ -11,18 +11,18 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://gitlab.com/mordil/swift-redi-stack.git", from: "1.0.0-alpha.7"),
+        .package(url: "https://gitlab.com/mordil/RediStack.git", from: "1.0.0-alpha.8"),
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-rc"),
     ],
     targets: [
         .target(name: "RedisKit", dependencies: [
+            "RediStack",
             .product(name: "AsyncKit", package: "async-kit"),
-            .product(name: "RediStack", package: "swift-redi-stack"),
             .product(name: "Logging", package: "swift-log"),
         ]),
         .testTarget(name: "RedisKitTests", dependencies: [
             .target(name: "RedisKit"),
-            .product(name: "RediStackTestUtils", package: "swift-redi-stack"),
+            .product(name: "RediStackTestUtils", package: "RediStack"),
         ]),
     ]
 )
